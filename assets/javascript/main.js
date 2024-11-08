41b084e58b0ab6bd055a17bcf3fa02ea1a908d48
Nabil Mouzouna <nabilmouzouna2001@gmail.com>
2024-11-08 23:31:34 +0100

    first commit

---
+++ assets/javascript/main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        const loaderWrapper = document.querySelector('.loader-wrapper');
+        const content = document.querySelector('.content');
+        
+        loaderWrapper.classList.add('fade-out');
+        
+        content.classList.add('visible');
+        
+        setTimeout(function() {
+            loaderWrapper.style.display = 'none';
+        }, 500);
+    }, 2000);
+});
